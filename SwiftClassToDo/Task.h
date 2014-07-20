18673ac8866717cb215402f6efa92b3d5e13e359
jsyoun <julie_youn@yahoo.com>
2014-07-19 17:12:51 -0700

    first commit

---
+++ SwiftClassToDo/Task.h
@@ -0,0 +1,13 @@
+//
+//  Task.h
+//  SwiftClassToDo
+//
+//  Created by Julie Youn on 7/12/14.
+//  Copyright (c) 2014 Julie Youn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Task : NSObject
+
+@end
---
+++ SwiftClassToDo/Task.m
@@ -0,0 +1,13 @@
+//
+//  Task.m
+//  SwiftClassToDo
+//
+//  Created by Julie Youn on 7/12/14.
+//  Copyright (c) 2014 Julie Youn. All rights reserved.
+//
+
+#import "Task.h"
+
+@implementation Task
+
+@end
